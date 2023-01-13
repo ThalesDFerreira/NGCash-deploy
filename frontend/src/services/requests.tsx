5d06a9ba98f15b94ccf3a3b03475fd768ec5943a
ThalesFerreira <thalesferreira190@gmail.com>
2023-01-13 17:58:15 -0300

    correcoes finais2

--- frontend/src/services/requests.tsx
+++ frontend/src/services/requests.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_PORT}`,
+  baseURL: 'https://ngcash-deploy-production.up.railway.app/',
 });
 
 export const setToken = (token: string) => {
