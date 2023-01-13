af4bcfc0d9ea8da56d3a5e41131df2a321fef5ca
ThalesFerreira <thalesferreira190@gmail.com>
2023-01-13 17:30:07 -0300

    testando deploy frontend

--- frontend/src/services/requests.tsx
+++ frontend/src/services/requests.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'ngcash-deploy-production.up.railway.app',
+  baseURL: `${process.env.REACT_APP_API_PORT}`,
 });
 
 export const setToken = (token: string) => {
